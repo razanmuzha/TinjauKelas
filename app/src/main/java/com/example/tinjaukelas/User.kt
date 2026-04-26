95eab5d6b131ce9f771910b3cff310a980a55ca6
FlintHassel <akmalcode843@gmail.com>
2026-04-26 17:30:16 +0700

    Update User.kt

--- app/src/main/java/com/example/tinjaukelas/User.kt
+++ app/src/main/java/com/example/tinjaukelas/User.kt
@@ -1,13 +1,7 @@
 package com.example.tinjaukelas
-
-<<<<<<< HEAD
-class User {
-}
-=======
 data class User(
     val id: Int = 0,
     val email: String,
     val password: String,
     val role: String
 )
->>>>>>> backendtesting
